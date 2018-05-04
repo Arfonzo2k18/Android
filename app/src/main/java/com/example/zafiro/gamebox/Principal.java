6eaf544d0553e359762265ee7aac755fec82eb93
Arfonzo2k18 <asgproducer@gmail.com>
2018-05-04 16:58:37 +0200

    Nuevos archivos
    Se avanza con el proyecto

--- app/src/main/java/com/example/zafiro/gamebox/Principal.java
+++ app/src/main/java/com/example/zafiro/gamebox/Principal.java
@@ -1,13 +1,41 @@
 package com.example.zafiro.gamebox;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class Principal extends AppCompatActivity {
+public class Principal extends AppCompatActivity implements View.OnClickListener{
+
+    ImageButton imbAyuda;
+    ImageButton imbAñadir;
+    ImageButton imbLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        imbAyuda = (ImageButton)findViewById(R.id.imbAyuda);
+        imbAñadir = (ImageButton)findViewById(R.id.imbAñadir);
+        imbLista = (ImageButton)findViewById(R.id.imbLista);
+
+        View viewimbAyuda = findViewById(R.id.imbAyuda);
+        viewimbAyuda.setOnClickListener(this);
+
+        View viewimbAñadir = findViewById(R.id.imbAñadir);
+        viewimbAñadir.setOnClickListener(this);
+
+        View viewimbLista = findViewById(R.id.imbLista);
+        viewimbLista.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==findViewById(R.id.imbAyuda).getId()) {
+            Intent Ayuda = new Intent(getApplicationContext(),Ayuda.class);
+            startActivity(Ayuda);
+        }
     }
 }
